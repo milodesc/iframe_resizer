6159a29ae4d0c7e80a638e10e02bba1decd4628f
Patrick Jensen <patrick.c.jensen@gmail.com>
2015-11-09 16:57:30 -0500

    Allow admins to configure logging and height calculation method.

--- js/iframe_resizer.js
+++ js/iframe_resizer.js
@@ -1,7 +1,11 @@
 (function ($) {
   Drupal.behaviors.iframe_resizer = {
     attach: function (context, settings) {
-      $('iframe', context).iFrameResize( {heightCalculationMethod:'lowestElement'} );
+      var options = {
+          log: settings.iframe_resizer.log == 1,
+          heightCalculationMethod: settings.iframe_resizer.heightCalculationMethod
+      };
+      $('iframe', context).iFrameResize(options);
     }
   };
 }(jQuery));
