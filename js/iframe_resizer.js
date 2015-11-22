56d45303ffcab1f5d108e9046d87ac60f1c0db48
Patrick Jensen <patrick.c.jensen@gmail.com>
2015-11-22 16:21:57 -0500

    Only apply the overriding options if the 'Override the default behavior of the iFrame Resizer library.' checkbox is checked.

--- js/iframe_resizer.js
+++ js/iframe_resizer.js
@@ -1,11 +1,14 @@
 (function ($) {
   Drupal.behaviors.iframe_resizer = {
     attach: function (context, settings) {
-      var options = {
-          log: settings.iframe_resizer_options.log == 1,
-          heightCalculationMethod: settings.iframe_resizer_options.heightCalculationMethod,
-          widthCalculationMethod: settings.iframe_resizer_options.widthCalculationMethod
-      };
+      var options = {};
+      if (settings.iframe_resizer_override_defaults) {
+        options = {
+            log: settings.iframe_resizer_options.log == 1,
+            heightCalculationMethod: settings.iframe_resizer_options.heightCalculationMethod,
+            widthCalculationMethod: settings.iframe_resizer_options.widthCalculationMethod
+        };
+      }
       $(settings.iframe_resizer_target_specifiers, context).iFrameResize(options);
     }
   };
