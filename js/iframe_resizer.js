0317ad794fc72bd6d15d4a8dc2037621fe30c7d6
Patrick Jensen <patrick.c.jensen@gmail.com>
2015-11-28 16:58:35 -0500

    Add ability to configure the bodyBackground option.

--- js/iframe_resizer.js
+++ js/iframe_resizer.js
@@ -3,11 +3,15 @@
     attach: function (context, settings) {
       var options = {};
       if (settings.iframe_resizer_override_defaults) {
+        if (settings.iframe_resizer_options.bodyBackground == '') {
+          settings.iframe_resizer_options.bodyBackground = null;
+        }
         options = {
             log: settings.iframe_resizer_options.log == 1,
             heightCalculationMethod: settings.iframe_resizer_options.heightCalculationMethod,
             widthCalculationMethod: settings.iframe_resizer_options.widthCalculationMethod,
-            autoResize: settings.iframe_resizer_options.autoResize == 1
+            autoResize: settings.iframe_resizer_options.autoResize == 1,
+            bodyBackground: settings.iframe_resizer_options.bodyBackground
         };
       }
       $(settings.iframe_resizer_target_specifiers, context).iFrameResize(options);
