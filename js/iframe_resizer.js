ddab14273c9b8efc458a9bbd6282cd62671261bd
Patrick Jensen <patrick.c.jensen@gmail.com>
2015-11-29 06:32:32 -0500

    Add ability to configure the interval option.

--- js/iframe_resizer.js
+++ js/iframe_resizer.js
@@ -9,6 +9,9 @@
         if (settings.iframe_resizer_options.bodyMargin == '') {
           settings.iframe_resizer_options.bodyMargin = null;
         }
+        if (settings.iframe_resizer_options.interval == '') {
+          settings.iframe_resizer_options.interval = 32;
+        }
         options = {
             log: settings.iframe_resizer_options.log == 1,
             heightCalculationMethod: settings.iframe_resizer_options.heightCalculationMethod,
@@ -16,7 +19,8 @@
             autoResize: settings.iframe_resizer_options.autoResize == 1,
             bodyBackground: settings.iframe_resizer_options.bodyBackground,
             bodyMargin: settings.iframe_resizer_options.bodyMargin,
-            inPageLinks: settings.iframe_resizer_options.inPageLinks == 1
+            inPageLinks: settings.iframe_resizer_options.inPageLinks == 1,
+            interval: parseInt(settings.iframe_resizer_options.interval)
         };
       }
       $(settings.iframe_resizer_target_specifiers, context).iFrameResize(options);
